73754c57886d28ba69b15ff734732039a60c7909
lechodiman <lechodiman@uc.cl>
2019-04-13 20:37:16 -0400

    Change template project

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,55 +1,24 @@
-var title = document.querySelector(".title");
-var courseFeatureElements = document.querySelectorAll(".course-feature");
-var button = document.querySelector("button");
+var pwaCard = document.querySelector('#pwa');
+var pwaCardContent = pwaCard.querySelector('.card__content');
+var pwaCardDetails = pwaCard.querySelector('.card__details');
+var detailsShown = false;
 
-navigator.serviceWorker.register("/sw.js");
-
-function animate() {
-  title.classList.remove("animate-in");
-  for (var i = 0; i < courseFeatureElements.length; i++) {
-    courseFeatureElements[i].classList.remove("animate-in");
+pwaCard.addEventListener('click', function (event) {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
   }
-  button.classList.remove("animate-in");
-
-  setTimeout(function() {
-    title.classList.add("animate-in");
-  }, 1000);
-
-  setTimeout(function() {
-    courseFeatureElements[0].classList.add("animate-in");
-  }, 3000);
-
-  setTimeout(function() {
-    courseFeatureElements[1].classList.add("animate-in");
-  }, 4500);
-
-  setTimeout(function() {
-    courseFeatureElements[2].classList.add("animate-in");
-  }, 6000);
-
-  setTimeout(function() {
-    courseFeatureElements[3].classList.add("animate-in");
-  }, 7500);
-
-  setTimeout(function() {
-    courseFeatureElements[4].classList.add("animate-in");
-  }, 9000);
-
-  setTimeout(function() {
-    courseFeatureElements[5].classList.add("animate-in");
-  }, 10500);
-
-  setTimeout(function() {
-    courseFeatureElements[6].classList.add("animate-in");
-  }, 12000);
-
-  setTimeout(function() {
-    button.classList.add("animate-in");
-  }, 13500);
-}
-
-animate();
-
-button.addEventListener("click", function() {
-  animate();
 });
