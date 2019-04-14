3b7df64d9cdadf62b4a7ddc960f9243715128f97
lechodiman <lechodiman@uc.cl>
2019-04-13 21:24:17 -0400

    Cache static resources

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,27 @@
 // Created under public directory so it has scope over all of my pages
-
 // This guy is event driven
+// The main usage is caching
+// the cache stores a key value pair of request-response
 
 // Self refers to the service worker process
-self.addEventListener("install", function() {
+self.addEventListener("install", function(event) {
   console.log("SW installed");
+  // wait until, because sw go idle after a certain amount of time and I don't want it to sleep while I cache data
+  event.waitUntil(
+    caches.open("static").then(cache => {
+      // cache.add("/");
+      // cache.add("/index.html");
+      // cache.add("/src/js/app.js");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/css/app.css",
+        "src/images/pwa.jpg",
+        "https://fonts.googleapis.com/css?family=Raleway:400,700"
+      ]);
+    })
+  );
 });
 
 // it triggers when all instances of the service worker are closed, ie, after user installed the pwa and closed all tabs related to the page
